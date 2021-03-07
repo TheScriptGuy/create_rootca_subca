f70dfd896a1274948b3d6c38ca51da4a2463f36f
Git <gituser@calvin.home.remotenode.org>
2021-03-06 23:06:39 -0800

    2021-03-06 - 23:06:39 - Updating all files.

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Script name:              cleanup.sh
+# Author:                   github@remotenode.org
+# Version:                  0.01
+# Date last modified:       2021/03/05
+# Description:              Remove configurations that were created.
+#
+# Changelog:                2021/03/05 - Initial publish.
+#
+
+
 echo "Removing all certificate authorities, revocation lists, certificates"
 
 rm -rf *ca *certs *crl
